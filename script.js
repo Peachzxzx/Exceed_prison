fd445a7af8d093ea8398db8812cc467827004767
025528078pP <peachzxzx@hotmail.com>
2019-07-11 12:15:21 +0700

    Test

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 url = "https://exceed.superposition.pknn.dev/data/15"
 
-function POST() {
+function POST_door() {
     let text = document.getElementById('Text_input').value
-    var data = {door: "" ,buzzer: ""}
-    fetch(url, {
+    var data = {door: false}
+    fetch(url + "/door", {
         method: 'POST',
         body: JSON.stringify(data),
         headers:{
         'Content-Type': 'application/json'}
     })
+}
+
+function POST_buzzer() {
+  let text = document.getElementById('Text_input').value
+  var data = {buzzer: false}
+  fetch(url + "/buzzer", {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers:{
+      'Content-Type': 'application/json'}
+  })
 }
 
 let lasttext = ''
@@ -23,6 +34,7 @@ setInterval(function () {
     if (lasttext != text){
     document.getElementById('main').innerHTML += `<h3>${text}</h3>`
     lasttext = text
+
 }
   });
 },1000)
